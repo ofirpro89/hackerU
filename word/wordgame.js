51329820711f35ac3af4c33451477fda879bfaf8
ofirpro89 <ofirpro89@gmail.com>
2025-03-01 21:47:47 +0200

    word

---
+++ word/wordgame.js
@@ -0,0 +1,47 @@
+let rowindex = 1;
+let colindex = 1;
+let currentWord = "";
+const words = ["שולחן", "פרחתי", "ספרים", "חיילים", "גיבור", "ארוחה", "נופים", "רכבת", "גזירה"];
+const rundomIndex = Math.floor(Math.random() * words.length ) +1;
+const word = words[rundomIndex];
+let chakeWord = words[rundomIndex].split("");
+
+
+console.log(chakeWord);
+
+let start = (object)=>{
+
+    let cell = document.querySelector("div[data-row='"+rowindex + "'][data-col='"+colindex+"']");
+    currentWord = currentWord + object.value;
+    cell.innerHTML = object.value;
+   
+    colindex++;
+  
+    if(colindex == 6){
+        if(currentWord === word){
+            alert("victori");
+        
+        }
+    
+
+
+        
+        if(currentWord ==! chakeWord){
+            styleCell = cell.style
+            cell.style.bgcolor = "red";
+        }
+
+        if(rowindex ==5){
+            alert("game Over");
+            return;
+        }
+
+        rowindex++;
+        colindex=1;
+
+    }
+
+}
+
+
+
